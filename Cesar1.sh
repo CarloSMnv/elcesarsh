7c913bee6cca35928256a325491988784d9bf10a
CarloSMnv <112900239+CarloSMnv@users.noreply.github.com>
2023-02-07 04:00:20 +0000

    Add files via upload

---
+++ Cesar1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define la frase a cifrar y el desplazamiento
+frase="HOLA MUNDO"
+desplazamiento=3
+
+# Convierte la frase a mayúsculas
+frase=`echo $frase | tr '[:lower:]' '[:upper:]'`
+
+# Cifra la frase usando el cifrado César
+cifrado=""
+for((i=0;i<${#frase};i++))
+do
+  char=${frase:$i:1}
+  # Si el carácter es una letra, cifralo
+  if [[ "$char" =~ [A-Z] ]]; then
+    ascii=$(printf "%d" "'$char")
+    ascii=$((ascii-65+desplazamiento))
+    ascii=$((ascii%26+65))
+    char=$(printf \\$(printf '%03o' $ascii))
+  fi
+  cifrado+="$char"
+done
+
+# Muestra el resultado
+echo "Frase original: $frase"
+echo "Frase cifrada: $cifrado"
